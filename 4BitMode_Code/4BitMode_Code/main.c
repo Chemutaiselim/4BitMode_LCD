e05cb816d4b9b838d691d51f9d4ecd2144e13157
Selim <selimchemutai19@gmail.com>
2021-11-06 16:36:01 +0300

    Create main.c

---
+++ 4BitMode_Code/4BitMode_Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * 4BitMode_Code.c
+ *
+ * Created: 06/11/2021 16:34:37
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
