6c9531432740c987b3d098bebafee0275abed284
Selim <selimchemutai19@gmail.com>
2021-11-06 17:09:08 +0300

    Update LCD.c

--- 4BitMode_Code/4BitMode_Code/LCD.c
+++ 4BitMode_Code/4BitMode_Code/LCD.c
@@ -12,50 +12,67 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define RS PC0
+//#define RS PC0
 #define RW PC1
-#define EN PC2
+//#define EN PC2
 #define LCD_Data_Port PORTD 
 #define LCD_Cmd_Port PORTC 
 #define LCD_Cmd_Dir DDRC 
 #define LCD_Data_Dir DDRD 
 
-/*
-void LCD_Cmd(unsigned char cmd){ 
-	LCD_Data_Port = cmd; 
-	LCD_Cmd_Port &= ~(1<<RS); // RS=0 command reg. 
-	LCD_Cmd_Port &= ~(1<<RW); // RW=0 Write operation 
-	_delay_us(1); 
-	LCD_Cmd_Port |= (1<<EN); // Enable pulse ON 
-	_delay_us(1); 
-	LCD_Cmd_Port &= ~(1<<EN); // Enable pulse OFF 
-	_delay_ms(2); 
-	}
+/*Useful pin and port definitions*/
+#define LCD_Dir DDRD
+#define LCD_Port PORTD
+#define RS PD0
+#define EN PD1
+
+/*LCD command write function*/
+void LCD_Cmd(unsigned char cmd){
+	/*Sending the first nibble of data (Higher 4 bits)*/
+	LCD_Port = (LCD_Port & 0x0F) | (cmd & 0xF0);
+	/* Sending upper nibble */
+	LCD_Port &= ~ (1<<RS); /* RS=0, command reg. */
+	LCD_Port |= (1<<EN); /* Enable pulse ON */
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN); /* Enable pulse OFF */
+	_delay_us(200);
+	/*Sending the second nibble of data (Lower 4 bits)*/
+	LCD_Port = (LCD_Port & 0x0F) | (cmd << 4);
+	/* Sending lower nibble */
+	LCD_Port |= (1<<EN); /* Enable pulse ON */
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN); /* Enable pulse OFF */
+	_delay_ms(2);
+}
 	
 //LCD data write function 
-void LCD_Char (unsigned char char_data){ 
-	LCD_Data_Port = char_data; 
-	LCD_Cmd_Port |= (1<<RS); // RS=1 Data reg. 
-	LCD_Cmd_Port &= ~(1<<RW); // RW=0 write operation  
-	_delay_us(1); 
-	LCD_Cmd_Port |= (1<<EN); // Enable Pulse ON 
-	_delay_us(1); 
-	LCD_Cmd_Port &= ~(1<<EN); // Enable Pulse ON  
-	_delay_ms(2); 
-	}
+void LCD_Char (unsigned char char_data){
+	/*Sending the first nibble of data (Higher 4 bits)*/
+	LCD_Port = (LCD_Port & 0x0F) | (char_data & 0xF0);
+	/* Sending upper nibble */
+	LCD_Port |= (1<<RS); /* RS=1, data reg. */
+	LCD_Port |= (1<<EN); /* Enable pulse ON */
+	_delay_us(1); LCD_Port &= ~ (1<<EN); /* Enable pulse OFF */
+	_delay_us(200);
+	/*Sending the second nibble of data (Lower 4 bits)*/
+	LCD_Port = (LCD_Port & 0x0F) | (char_data << 4);
+	/* Sending lower nibble */
+	LCD_Port |= (1<<EN); /* Enable pulse ON */
+	_delay_us(1);
+	LCD_Port &= ~ (1<<EN); /* Enable pulse OFF */
+	_delay_ms(2);
+}
 	
 // LCD Initialize function  
-void LCD_Init (void){ 
-	LCD_Cmd_Dir = 0xFF; // Make LCD command port direction as output pins 
-	LCD_Data_Dir = 0xFF; // Make LCD data port direction as output pins 
-	_delay_ms(50); // LCD Power ON delay always >15ms 
-	LCD_Cmd(0x02); // Return display to its home position 
-	LCD_Cmd(0x38); // Initialization of 16X2 LCD in 8bit mode  
-	LCD_Cmd(0x0F); // Display ON Cursor Blinking 
-	LCD_Cmd(0x06); /* Auto Increment cursor 
-	LCD_Cmd(0x01); // Clear display  
-	}
-	*/
+void LCD_Init (void){
+	LCD_Dir = 0xFF; /* Make LCD command port direction as output pins*/
+	_delay_ms(20); /* LCD Power ON delay always > 15ms */
+	LCD_Cmd(0x02); /* Return display to its home position */
+	LCD_Cmd(0x28); /* 2 line 4bit mode */
+	LCD_Cmd(0x0C); /* Display ON Cursor OFF */
+	LCD_Cmd(0x06); /* Auto Increment cursor */
+	LCD_Cmd(0x01); /* Clear display */
+}
 	
 /*Clear LCD Function*/ 
 void LCD_Clear(void){ 
